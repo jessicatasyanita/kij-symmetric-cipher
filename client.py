78654cf72fa062a1666221f6ba6ccdb05cfe1072
dyahputrin <putri.nariswari@gmail.com>
2022-10-08 21:02:39 +0700

    change file transfer method and add aes

--- client.py
+++ client.py
@@ -1,73 +1,117 @@
-
-import socket
+import socket,os
+import pyAesCrypt
 from Crypto.Cipher import AES
-import base64
-import sys
+import io
+from Crypto import Random
 
-# from psutil import long
+# define ip and port
+HOST = 'localhost'
+PORT = 8080
+# socket configuration
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 
-key = "This is a key123"
-iv = "This is an IV456"
-# obj = AES.new(key, AES.MODE_CBC, iv)
-obj = AES.new('This is a key123'.encode("utf8"), AES.MODE_CBC, 'This is an IV456'.encode("utf8"))
+# set buffersize and password for client-server encryption
+bufferSize = 1024
+password = 'kijc-its-2022'
 
-def Main():
-    
-    if len(sys.argv) < 3:
-        print("Please Enter IP Address and Port Number")
-        # print(sys.argv)
-        # return
-    
-    # host = sys.argv[1]
-    # print(host)
-    host = '0.0.0.0'
-    # port = int(sys.argv[2])
-    # print(port)
-    port = 8080
-    
-    s = socket.socket()
-    s.connect((host, port))
-    lineNo = 1
+# do data encryption for client-server
+def encryptData(msg):
+	pbdata = str.encode(msg)
+	fileInput = io.BytesIO(pbdata)
+	fileCipher = io.BytesIO()
+	pyAesCrypt.encryptStream(fileInput, fileCipher, password, bufferSize)
+	dataToSend = fileCipher.getvalue()
+	return dataToSend
 
-    filename = input("Filename? -> ")
-    if filename != 'q':
-        s.send(filename)
-        data = s.recv(1024)
-        if data[:6] == 'EXISTS':
-            fileSize = int(data[6:])
-            message = input("File exists, " + str(fileSize) +" Bytes, download? (Y/N)? -> ")
-            if message == 'Y':
-                s.send("OK")
-                f = open('new_'+filename, 'wb')
-                data = s.recv(1024)
-                totalRecv = len(data)
-                lineNo += 1
-                f.write(data)
-                print("{0:.2f}".format((totalRecv/float(fileSize))*100)+ "% Done")
-                
-                while (fileSize - totalRecv) >= 1024:
-                
-                    data = s.recv(1024)
-                    originalData = obj.decrypt(data)
-                    totalRecv += 1024
-                    lineNo += 1
-                    f.write(originalData)
-                    print("{0:.2f}".format((totalRecv/float(fileSize))*100)+ "% Done")
-                
-                if (fileSize - totalRecv) < 1024 and totalRecv < fileSize:
-                
-                    data = s.recv(1024)
-                    totalRecv += (fileSize - totalRecv)
-                    f.write(data)
-                    print("{0:.2f}".format((totalRecv/float(fileSize))*100)+ "% Done")
-            
-                print("{0:.2f}".format((totalRecv/float(fileSize))*100)+ "% Done")
-                print("Download Completed")
-                f.close()
-        else:
-            print("File Does Not Exist!")
-    s.close()
+# do data decryption for client-server
+def decryptData(msg):
+	fileCipher = io.BytesIO()
+	fDec = io.BytesIO()
 
+	fileCipher = io.BytesIO(msg)
+	ctlen = len(fileCipher.getvalue())
+	fileCipher.seek(0)
 
-if __name__ == '__main__':
-    Main()
+	pyAesCrypt.decryptStream(fileCipher, fDec, password, bufferSize, ctlen)
+	decrypted = str(fDec.getvalue().decode())
+	return decrypted
+
+# do file encryption with AES
+def encryptFileAES(filename):
+	blockSize = 64*1024
+	outputFile = "(aes)"+filename
+	filesize = str(os.path.getsize(filename)).zfill(16)
+	IV = Random.new().read(16)
+	key = b'0123456789ABCDEF'
+	encryptor = AES.new(key, AES.MODE_CBC, IV)
+
+	#read binary
+	with open(filename, 'rb') as inFile:
+		#write binary 
+		with open(outputFile, 'wb') as outFile:
+			outFile.write(filesize.encode('utf-8'))
+			outFile.write(IV)
+			while True:
+				block = inFile.read(blockSize)
+				if len(block) == 0:
+					break
+				elif len(block)%16 != 0:
+					block += b' '*(16-(len(block)%16))
+				outFile.write(encryptor.encrypt(block))
+	try:
+		return 'Encryption process for file ' + filename + ' complete.\n'
+	except:
+		return 'Oops! There is an error while encrypting.\n'
+
+# Decrypt file AES
+def decryptFileAES(filename):
+	blockSize = 64*1024
+	outputFile = "(dec)" + filename
+
+	with open(filename, 'rb') as inFile:
+		filesize = int(inFile.read(16))
+		IV = inFile.read(16)
+		key = b'0123456789ABCDEF'
+		decryptor= AES.new(key, AES.MODE_CBC, IV)
+
+		with open(outputFile, 'wb') as outFile:
+			while True:
+				block = inFile.read(blockSize)
+				if len(block) == 0:
+					break
+				outFile.write(decryptor.decrypt(block))
+			outFile.truncate(filesize)
+	try:
+		return 'Decryption process for file ' + filename + ' complete.\n'
+	except:
+		return 'Oops! There is an error while decrypting.\n'
+
+s.sendall(encryptData('SYMMETRIC CIPHER\n'))
+s.sendall(encryptData('EOFX'))
+
+while 1:
+	data = s.recv(1024)
+	decrypted = decryptData(data)
+	# quit program
+	if decrypted == "quit":
+		print('\nProcess end, thankyou!')
+		break
+	# do the encryption or decryption for file
+	elif decrypted[:15] == "encryptFileAES " or decrypted[:15] == "decryptFileAES ":
+		try:
+			args = dict(e.split('=') for e in decrypted[15:].split(', '))
+			if len(args['file']): pass
+			else: args = 0
+		except:
+			args = 0
+			s.sendall(encryptData('Error: invalid arguments.\n'))
+		if args:
+			if (decrypted[:15] == "encryptFileAES "):
+				s.sendall(encryptData(encryptFileAES(args['file'])))
+			if (decrypted[:15] == "decryptFileAES "): 
+				s.sendall(encryptData(decryptFileAES(args['file'])))
+		s.sendall(encryptData('EOFX'))
+	else:
+		s.sendall(encryptData('Command Not Found\n'))
+s.close()
--- server.py
+++ server.py
@@ -1,72 +1,59 @@
-
 import socket
-import threading
-import os
-from Crypto.Cipher import AES
-import base64
-import sys
+import pyAesCrypt
+import io
 
-key = "This is a key123"
-iv = "This is an IV456"
-# obj = AES.new(key, AES.MODE_CBC, iv)
-obj = AES.new('This is a key123'.encode("utf8"), AES.MODE_CBC, 'This is an IV456'.encode("utf8"))
+# socket configuration
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c.bind(('0.0.0.0', 8080))
+c.listen(1)
 
-def RetrFile(name, sock):
-    lineNo = 0
-    filename = sock.recv(1024)
-    if os.path.isfile(filename):
-        sock.send("EXISTS " + str(os.path.getsize(filename)))
-        fileSize = int(os.path.getsize(filename))
-        userResponse = sock.recv(1024)
-        if userResponse[:2] == 'OK':
-            with open(filename, 'rb') as f:
-                totalSent = 0
-                bytesToSend = f.read(1024)
-                print(lineNo," original Data:",bytesToSend)
-                lineNo += 1
-                totalSent += 1024
-                sock.send(bytesToSend)
-                print("{0:.2f}".format((totalSent/float(fileSize))*100)+ "% Done")
-    
-                while (fileSize - totalSent) >= 1024 and totalSent < fileSize:
-                    
-                    bytesToSend = f.read(1024)
-                    encryptedData = obj.encrypt(bytesToSend)
-                    sock.send(encryptedData)
-                    totalSent += 1024
-                    print("{0:.2f}".format((totalSent/float(fileSize))*100)+ "% Done")
-            
-                if (fileSize - totalSent) < 1024 and totalSent < fileSize:
-                    
-                    bytesToSend = f.read(1024)
-                    sock.send(bytesToSend)
-                    totalSent += (fileSize - totalSent)
-                    print("{0:.2f}".format((totalSent/float(fileSize))*100)+ "% Done")
-                
-                print("File Transfer Completed")
-                
-        sock.close()
-    else:
-        sock.send("ERR ")
+# set buffersize and password for client-server encryption
+bufferSize = 1024
+password = 'kijc-its-2022'
 
-    sock.close()
+s,a = c.accept()
 
-def Main():
-    host = ''
-    port = 8080
+# do data encryption for client-server
+def encryptData(msg):
+	pbdata = str.encode(msg)
+	fileInput = io.BytesIO(pbdata)
+	fileCipher = io.BytesIO()
+	# try use different library for AES
+	pyAesCrypt.encryptStream(fileInput, fileCipher, password, bufferSize)
+	dataToSend = fileCipher.getvalue()
+	return dataToSend
 
-    s = socket.socket()
-    s.bind((host,port))
-    s.listen(5)
+# do data decryption for client-server
+def decryptData(msg):
+	# initialize
+	fileCipher = io.BytesIO()
+	fDec = io.BytesIO()
 
-    print ("Server Listening on Port:", port)
-    while True:
-        c, addr = s.accept()
-        print("client connedted ip:<" + str(addr) + ">")
-        t = threading.Thread(target=RetrFile, args=("RetrThread", c))
-        t.start()
+	fileCipher = io.BytesIO(msg)
+	ctlen = len(fileCipher.getvalue())
+	fileCipher.seek(0)
 
-    s.close()
+	pyAesCrypt.decryptStream(fileCipher, fDec, password, bufferSize, ctlen)
+	decrypted = str(fDec.getvalue().decode())
+	return decrypted
 
-if __name__ == '__main__':
-    Main()
+while True:
+	# receive the data 
+	data = s.recv(1024)
+	try:
+		decrypted = decryptData(data)
+	except ValueError:
+		print('> Oops! Decryption process erorr.\n')
+		pass
+	# check for eof or command
+	if decrypted.endswith("EOFX") == True:
+		# get the next command
+		nextcmd = input("[command]: ")
+		if nextcmd == 'quit':
+			print('\nProcess end, thankyou!')
+			s.send(encryptData(nextcmd))
+			break
+		else: s.send(encryptData(nextcmd))	
+	# we haven't reached EOF, print
+	else:
+		print(decrypted, end = '')
